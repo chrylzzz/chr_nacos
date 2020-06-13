d149cbd9e231dfbfd9b2e628791a9d826c25e593
chryl <cyl5yy412@gmail.com>
2020-06-14 00:31:13 +0800

    init

--- nacosconfig/service1/src/main/java/com/chryl/Service1Application.java
+++ nacosconfig/service1/src/main/java/com/chryl/Service1Application.java
@@ -3,11 +3,14 @@ package com.chryl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Created by Chr.yl on 2020/6/14.
+ *
+ * @author Chr.yl
+ */
 @SpringBootApplication
-public class NacosSimpleDemoApplication {
-
+public class Service1Application {
     public static void main(String[] args) {
-        SpringApplication.run(NacosSimpleDemoApplication.class, args);
+        SpringApplication.run(Service1Application.class, args);
     }
-
 }
--- nacosconfig/service2/src/main/java/chryl/Service2Application.java
+++ nacosconfig/service2/src/main/java/chryl/Service2Application.java
@@ -1,13 +1,16 @@
-package com.chryl;
+package chryl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Created by Chr.yl on 2020/6/14.
+ *
+ * @author Chr.yl
+ */
 @SpringBootApplication
-public class NacosSimpleDemoApplication {
-
+public class Service2Application {
     public static void main(String[] args) {
-        SpringApplication.run(NacosSimpleDemoApplication.class, args);
+        SpringApplication.run(Service2Application.class, args);
     }
-
 }
