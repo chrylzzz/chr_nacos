94b25da0acd7629dd5ee29c51608dc87685557c1
chryl <cyl5yy412@gmail.com>
2020-06-14 00:31:41 +0800

    springboot 基本项目创建

--- nacosconfig/service1/src/main/java/com/chryl/Service1Application.java
+++ nacosconfig/service1/src/main/java/com/chryl/Service1Application.java
@@ -12,5 +12,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Service1Application {
     public static void main(String[] args) {
         SpringApplication.run(Service1Application.class, args);
+
     }
 }
