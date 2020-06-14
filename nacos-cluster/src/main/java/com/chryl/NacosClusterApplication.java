0b2cf9eacba2f4599d95873bdb795f8fafa92fbf
chryl <cyl5yy412@gmail.com>
2020-06-14 16:28:59 +0800

    nacos 集群

--- nacos-cluster/src/main/java/com/chryl/NacosClusterApplication.java
+++ nacos-cluster/src/main/java/com/chryl/NacosClusterApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @SpringBootApplication
+@RestController
 public class NacosClusterApplication {
 
 	public static void main(String[] args) {
--- nacos-cluster/src/test/java/com/chryl/NacosClusterApplicationTests.java
+++ nacos-cluster/src/test/java/com/chryl/NacosClusterApplicationTests.java
@@ -1,5 +1,6 @@
 package com.chryl;
 
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
